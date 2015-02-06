91e487fff216325d80051e3f24062824bb69111a
Seth Pollack <teampollack@gmail.com>
2015-02-06 15:18:35 -0500

    fix tests

--- test/bugzscout_test.js
+++ test/bugzscout_test.js
@@ -12,9 +12,21 @@ describe("bugzscout", function() {
 				new BugzScout({
 					user: "my user",
 					project: "my project",
+					area: "my area",
+					description: "testing 123"
+				});
+			}).to.throw(Error);
+
+		});
+
+		it("should fail without a description", function() {
+			expect(function() {
+				new BugzScout({
+					user: "my user",
+					project: "my project",
 					area: "my area"
 				});
-			}).to.throw(BugzScoutError);
+			}).to.throw(Error);
 
 		});
 
@@ -23,9 +35,10 @@ describe("bugzscout", function() {
 				new BugzScout({
 					url: "http://your.fogbugz.url/scoutSubmit.asp",
 					project: "my project",
-					area: "my area"
+					area: "my area",
+					description: "testing 123"
 				});
-			}).to.throw(BugzScoutError);
+			}).to.throw(Error);
 
 		});
 
@@ -34,9 +47,10 @@ describe("bugzscout", function() {
 				new BugzScout({
 					url: "http://your.fogbugz.url/scoutSubmit.asp",
 					user: "my user",
-					area: "my area"
+					area: "my area",
+					description: "testing 123"
 				});
-			}).to.throw(BugzScoutError);
+			}).to.throw(Error);
 		});
 
 		it("should fail without a area", function() {
@@ -44,9 +58,10 @@ describe("bugzscout", function() {
 				new BugzScout({
 					url: "http://your.fogbugz.url/scoutSubmit.asp",
 					project: "my project",
-					user: "my user"
+					user: "my user",
+					description: "testing 123"
 				});
-			}).to.throw(BugzScoutError);
+			}).to.throw(Error);
 		});
 	});
 
@@ -66,21 +81,19 @@ describe("bugzscout", function() {
 					url: "http://your.fogbugz.url/scoutSubmit.asp",
 					project: "my project",
 					user: "my user",
-					area: "my area"
+					area: "my area",
+					description: "testing 123"
 				});
 
 			var expectedResponse = {
-				body: {
+				body: JSON.stringify({
 					ScoutUserName: "my user",
 					ScoutProject: "my project",
 					ScoutArea: "my area",
-					Description: "",
-					ForceNewBug: "",
-					Extra: "",
-					Email: "",
-					ScoutDefaultMessage: "",
+					Description: "testing 123",
+					ForceNewBug: false,
 					FriendlyResponse: 0
-				},
+				}),
 				url: "http://your.fogbugz.url/scoutSubmit.asp"
 			};
 

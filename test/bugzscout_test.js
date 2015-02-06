4152bc5c6e42ce17093cef9bdf4ac98e9de22587
Seth Pollack <spollack@qualitybath.com>
2015-02-06 11:31:16 -0500

    add more tests

--- test/bugzscout_test.js
+++ test/bugzscout_test.js
@@ -3,21 +3,14 @@ sinon       = require('sinon'),
 expect      = require('chai').expect,
 BugzScout = require('../lib/bugzscout');
 
-describe("bugzscout", function(){
-	var sandbox;
-	beforeEach(function(){
-		sandbox = sinon.sandbox.create();
-		sandbox.stub(request, "post");
-	});
+describe("bugzscout", function() {
 
-	afterEach(function(){
-		sandbox.restore();
-	});
+	describe("constructor", function() {
 
-	describe("constructor", function(){
-		it("should fail without a user", function(){
-			expect(function(){
+		it("should fail without a url", function() {
+			expect(function() {
 				new BugzScout({
+					user: "my user",
 					project: "my project",
 					area: "my area"
 				});
@@ -25,18 +18,31 @@ describe("bugzscout", function(){
 
 		});
 
-		it("should fail without a project", function(){
-			expect(function(){
+		it("should fail without a user", function() {
+			expect(function() {
 				new BugzScout({
+					url: "http://your.fogbugz.url/scoutSubmit.asp",
+					project: "my project",
+					area: "my area"
+				});
+			}).to.throw(BugzScoutError);
+
+		});
+
+		it("should fail without a project", function() {
+			expect(function() {
+				new BugzScout({
+					url: "http://your.fogbugz.url/scoutSubmit.asp",
 					user: "my user",
 					area: "my area"
 				});
 			}).to.throw(BugzScoutError);
 		});
 
-		it("should fail without a area", function(){
-			expect(function(){
+		it("should fail without a area", function() {
+			expect(function() {
 				new BugzScout({
+					url: "http://your.fogbugz.url/scoutSubmit.asp",
 					project: "my project",
 					user: "my user"
 				});
@@ -44,7 +50,43 @@ describe("bugzscout", function(){
 		});
 	});
 
-	describe("submit", function(){
+	describe("submit", function() {
+		var sandbox;
+		beforeEach(function() {
+			sandbox = sinon.sandbox.create();
+			sandbox.stub(request, "post");
+		});
+
+		afterEach(function() {
+			sandbox.restore();
+		});
+
+		it("should pass info to request", function() {
+			var bugzscout = new BugzScout({
+					url: "http://your.fogbugz.url/scoutSubmit.asp",
+					project: "my project",
+					user: "my user",
+					area: "my area"
+				});
+
+			var expectedResponse = {
+				body: {
+					ScoutUserName: "my user",
+					ScoutProject: "my project",
+					ScoutArea: "my area",
+					Description: "",
+					ForceNewBug: "",
+					Extra: "",
+					Email: "",
+					ScoutDefaultMessage: "",
+					FriendlyResponse: 0
+				},
+				url: "http://your.fogbugz.url/scoutSubmit.asp"
+			};
+
+			bugzscout.submit();
+			sinon.assert.calledWith(request.post, expectedResponse);
+		});
 
 	});
 
